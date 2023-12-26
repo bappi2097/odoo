de7bb62b62f373c59f7af78ef2700bede6615cdd
Bappi Saha <bappi.saha@bjitgroup.com>
2023-12-26 11:31:19 +0600

    estate account added

---
+++ odoo-docker/custom-addons/estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Real Estate Accounting",
+    'author': "Bappi Saha",
+    'category': "Real Estate/Brokerage",
+    'sequence': 0,
+    "depends": [
+        "estate",
+        "account"
+    ],
+    "auto_install": True,
+    'images': ['static/description/icon.png'],
+}
